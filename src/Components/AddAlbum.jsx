bcde61507761028a9769b8667c21763857a837bd
SabeehaKT <kampalasabeeha@gmail.com>
2024-06-08 00:41:18 +0530

    AlbumApp with React

---
+++ src/Components/AddAlbum.jsx
@@ -0,0 +1,52 @@
+import React from 'react'
+import NavBar from './NavBar'
+
+const AddAlbum = () => {
+  return (
+    <div>
+        <NavBar/>
+        <div className="container">
+            <div className="row">
+                <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                    <div className="row g-3">
+                    <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                            <label htmlFor="" className="form-label">UserName</label>
+                            <input type="text" className="form-control" />
+                        </div>
+                        <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                            <label htmlFor="" className="form-label">Album Name</label>
+                            <input type="text" className="form-control" />
+                        </div>
+                        <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                            <label htmlFor="" className="form-label">Email ID</label>
+                            <input type="text" className="form-control" />
+                        </div>
+                        <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                            <label htmlFor="" className="form-label">Topic</label>
+                            <input type="text" className="form-control" />
+                        </div>
+                        <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                            <label htmlFor="" className="form-label">Measured in</label>
+                            <select name="" id="" className="form-control">
+                                <option value="Pixel">Pixel</option>
+                                <option value="Resolution">Resolution</option>
+                                <option value="Ratio">Ratio</option>
+                                <option value="PrintSize">PrintSize</option>
+                            </select>
+                        </div>
+                        <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                            <label htmlFor="" className="form-label">Upload</label>
+                            <input type="file" name="" id="" className="form-control" />
+                        </div>
+                        <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                            <button className="btn btn-success">Add</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default AddAlbum
